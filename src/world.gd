57ed8c163afaae36901b423151a3cf61669ddd4e
Tarık Demirören <tarekarez5652@gmail.com>
2022-12-10 22:28:38 +0300

    restart button added

--- src/world.gd
+++ src/world.gd
@@ -10,5 +10,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("restart"):
+		get_tree().change_scene("res://src/world.tscn")
+	if Input.is_action_just_pressed("pause"):
+		pass #TODO
+	pass
