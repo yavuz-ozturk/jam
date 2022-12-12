ad71330c9ded0d8ac5252689e3db45bd7d771bcb
Tarık Demirören <tarekarez5652@gmail.com>
2022-12-12 16:30:32 +0300

    pause added

--- src/world.gd
+++ src/world.gd
@@ -4,9 +4,12 @@ extends Node2D
 # var a: int = 2
 # var b: String = "text"
 
+onready var layer : CanvasLayer = $PauseLayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	$Pause.custom_viewport = $Space_ship/Camera
+	$Pause/Pause_Scene.hide()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,5 +17,13 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("restart"):
 		get_tree().change_scene("res://src/world.tscn")
 	if Input.is_action_just_pressed("pause"):
-		pass #TODO
+		get_tree().paused = not get_tree().paused
+		if get_tree().paused:
+			$Pause/Pause_Scene.show()
+		else:
+			$Pause/Pause_Scene.hide()
 	pass
+
+
+
+
