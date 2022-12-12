6d0b2407037bf641eba378d604b9361fb84c2f11
yavuz-ozturk <71942103+yavuz-ozturk@users.noreply.github.com>
2022-12-12 16:39:11 +0300

    Revert "helo"
    This reverts commit 9cf1375e18e482bd60e0c1d1658f2a8bf1ea4c4e.

--- src/space_ship.gd
+++ src/space_ship.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 class_name SpaceShip
 
+var no_halo = load("res://assets/spaceship.png")
+var halo = load("res://assets/spaceship_GO.png")
+
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
@@ -77,6 +80,11 @@ func throttler() -> void:
 		
 	if Input.is_action_just_pressed("accelerate"):
 		$JetPropel.play()
+	
+	if Input.is_action_pressed("accelerate"):
+		$SpaceShipTemp.texture = halo
+	else:
+		$SpaceShipTemp.texture = no_halo
 
 		
 func throttle_stop(delta: float) -> void:
